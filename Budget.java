f1c94fba7d9010a7b395672b7b9a6297e85a725b
ibattish <ibattish@hotmail.com>
2022-10-15 18:09:25 -0400

    added comments

--- Budget.java
+++ Budget.java
@@ -3,26 +3,35 @@ public class Budget {
     double discretionarySpend;
     double essentialSpend;
 
+    /*Creates a new Budget. User inputs the percentage of their discretionary
+    income they want to donate. Essential and discretionary spending are
+    initialized at 0.*/
     public Budget(double percentIn) {
         percentDonate = percentIn;
         essentialSpend = 0;
         discretionarySpend = 0;
     }
 
+    /*Increases the amount of essential spending by the amount specified
+    * by the user*/
     double addEssential(double spentAmount) {
         return essentialSpend+spentAmount;
     }
 
+    /*Increases the discretionary spending by the amount specified by
+    * the user*/
     double addDiscretionary(double spentAmount){
         return discretionarySpend+spentAmount;
     }
 
+    /*Calculates the amount that the user should donate to charity.*/
     double amountToDonate() {
         double decimal = percentDonate/100;
         return discretionarySpend*decimal;
     }
 
-    void clear() {
+    /*At the end of the month the budget resets.*/
+    void reset() {
         percentDonate = 0;
         discretionarySpend = 0;
         essentialSpend = 0;

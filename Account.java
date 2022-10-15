6c8a642d422b4dac0d89c1a1908299e2c90fa42b
ibattish <ibattish@hotmail.com>
2022-10-15 18:09:44 -0400

    added comments

--- Account.java
+++ Account.java
@@ -2,7 +2,7 @@ public class Account {
     String name;
     Budget budget;
 
-    public Account (String nameIn, Budget budgetIn){
+    public Account (String nameIn, Budget budgetIn) {
         name = nameIn;
         budget = budgetIn;
     }

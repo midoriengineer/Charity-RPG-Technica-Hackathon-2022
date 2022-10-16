35501e9a0f962aafb4e855f5d2c1d2e11645bed0
ibattish <ibattish@hotmail.com>
2022-10-15 20:54:12 -0400

    changes

---
+++ HTMLsite/javascripts/functions.js
@@ -0,0 +1,9 @@
+var points = 0;
+var percentagedDonated= 0;
+
+function increasePercentage() {
+    percentageDonated = //percentage donated as seen in budget
+}
+function increasePoints() {
+    points = percentageDonated*100;
+}
